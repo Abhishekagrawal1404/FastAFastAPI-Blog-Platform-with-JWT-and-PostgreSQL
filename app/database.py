6de79d50c5fcd784549caf043c828654397cd914
Abhishek Agrawal <104071590+Abhishekagrawal1404@users.noreply.github.com>
2023-05-31 08:41:41 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,24 +1,15 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost/db_name"
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+DATABASE_URL = "postgresql://user:password@localhost/db_name"
 
-Base = declarative_base()
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-def initialize_database():
-    Base.metadata.create_all(bind=engine)
-DATABASE_URL = "postgresql://user:password@host:port/db_name"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
+
+
 def get_db():
     db = SessionLocal()
     try:
