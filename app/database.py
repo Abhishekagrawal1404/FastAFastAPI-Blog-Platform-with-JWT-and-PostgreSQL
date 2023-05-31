6853d1915ae927bec97ca87df63a67d97fdcf82a
Abhishek Agrawal <104071590+Abhishekagrawal1404@users.noreply.github.com>
2023-05-31 08:38:28 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -15,3 +15,13 @@ def get_db():
         db.close()
 def initialize_database():
     Base.metadata.create_all(bind=engine)
+DATABASE_URL = "postgresql://user:password@host:port/db_name"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
